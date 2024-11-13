6ca1f5cb43d19524eea1863cc40bf4300a46e27a
Nikita Shah <nikita12.shah@gmail.com>
2024-11-13 22:36:26 +0530

    Grammar and Syntax.

---
+++ GrammarAndSyntax.js
@@ -0,0 +1,7 @@
+// Declare two variables
+let coding = "fun";
+let Coding = "mysterious";
+
+// Output variables using console.log()
+console.log(coding);    // 'fun'
+console.log(Coding);    // 'mysterious'
