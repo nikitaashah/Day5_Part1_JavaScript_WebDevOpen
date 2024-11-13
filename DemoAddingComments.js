1d47c55d5e226670f33170eab7df2dd8145471e4
Nikita Shah <nikita12.shah@gmail.com>
2024-11-13 22:22:00 +0530

    Demo - Adding Comments.

---
+++ DemoAddingComments.js
@@ -0,0 +1,17 @@
+// Adding Comments
+// Add a single-line and a multi-line comment where they should be, respectively.
+// If the code runs without an error, you're on the right spot! If you need to see the solution:
+// https://codepen.io/manish-poduval/pen/LYdeOLV?editors=0010
+
+//I should be a single-line comment
+
+console.log("hello");
+console.log("world");
+
+/*
+    I should be a multi-line comment.
+    The function below will print out a 
+    message for the user.
+*/
+
+console.log("I like to code and add comments ");
