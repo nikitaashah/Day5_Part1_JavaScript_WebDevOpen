55aca971c01c695a48353e4d8112b8b0683979c3
Nikita Shah <nikita12.shah@gmail.com>
2024-11-13 22:42:50 +0530

    Demo - Declare Variables.

---
+++ DemoDeclareVariables.js
@@ -0,0 +1,8 @@
+// Your First Assignment!
+// Declare two variables here
+let first = "hi";
+let second = "bye";
+
+// output your variables using console.log()
+console.log(first);
+console.log(second);
