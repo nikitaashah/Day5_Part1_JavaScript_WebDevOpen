a59a7701a33336da71176e38c3747ecbd7b868ed
Nikita Shah <nikita12.shah@gmail.com>
2024-11-13 21:13:07 +0530

    Adding comments practice.

---
+++ comments.js
@@ -0,0 +1,9 @@
+// put // JavaScript will ignore this line - but a human might read it!
+// This code won't _do_ anything. It's just for explanation. your code here.
+
+// a one-line comment.
+
+/*
+    this is a longer,
+    multi-line comment
+*/
