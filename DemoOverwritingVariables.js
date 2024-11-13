66e2b07e54f0302b73c76bac0d85d09f3135860f
Nikita Shah <nikita12.shah@gmail.com>
2024-11-13 22:54:23 +0530

    Demo - Overwriting Variables.

---
+++ DemoOverwritingVariables.js
@@ -0,0 +1,12 @@
+// Overwriting Variables
+// Set the variable 'coding' to "weird"
+let coding = "weird";
+
+// Print the variable 'coding' to your console
+console.log("coding is " + coding);     // -> "coding is weird"
+
+// Set 'coding' to "cool and fun"
+coding = "cool and fun";
+
+// Print the variable 'coding' to your console again
+console.log("coding is " + coding);     // -> "coding is cool and fun"
