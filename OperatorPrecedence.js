b0c700b1b3762070fc468f3a319894e8bb7dbab9
Nikita Shah <nikita12.shah@gmail.com>
2024-11-14 20:28:26 +0530

    JavaScript Operator Precedence.

---
+++ OperatorPrecedence.js
@@ -0,0 +1,6 @@
+const i = 10 + (5 * 2 ** 3) / 4 - 6;
+//  === 10 + 5 * 8 / 4 - 6 <== start with the exponents (2 ** 3)
+//  === 10 + 40 / 4 - 6 <== then multiplication (5 * 8)
+//  === 10 + 10 - 6 <== then division (40 / 4)
+//  === 20 - 6 <== then addition (10 + 10 )
+//  ==> 14 <== and finally finish with subtraction (20 - 6)
