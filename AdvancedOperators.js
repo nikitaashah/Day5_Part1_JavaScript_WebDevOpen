a0a382a14d1e9f33462e60f30518cd7668845ab4
Nikita Shah <nikita12.shah@gmail.com>
2024-11-14 20:05:17 +0530

    Advanced Operators for Numbers Data Type.

---
+++ AdvancedOperators.js
@@ -0,0 +1,27 @@
+// Exponentiation
+console.log(2 ** 5);
+// 2 * 2 * 2 * 2 * 2
+// => 32
+
+
+// Modulo
+// 4 / 2 = 2
+console.log(4 / 2);
+// With a remainder of 0
+console.log(4 % 2);
+// 7 / 2 = 3.5
+console.log(7 / 2);
+// With a remainder of 1
+console.log(7 % 2);
+// If a number modulus other number is equal to 0, it is a multiple of "other number"
+// 8 is indeed a multiple of 2!
+console.log(8 % 2 === 0);   // -> True
+// 9 is not a multiple of 2!
+console.log(9 % 2 === 0);   // -> False
+
+
+// Assignment Operators
+let myAge = 25;
+
+myAge += 1;
+console.log(myAge);
