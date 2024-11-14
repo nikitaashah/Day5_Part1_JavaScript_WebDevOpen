3afb78d1f0ebc28a7b64d14f05bd1eadd420bc8d
Nikita Shah <nikita12.shah@gmail.com>
2024-11-14 17:40:06 +0530

    Number as a data type.

---
+++ NumberDataType.js
@@ -0,0 +1,10 @@
+// Integers
+let age = 34;
+
+// Floating-point numbers
+let price = 12.99;
+
+// NaN (Not A Number)
+const names = 'Sandra';
+const whatIsThis = names / 2;
+console.log(whatIsThis);    // ==> NaN
